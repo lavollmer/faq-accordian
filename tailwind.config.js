63aa3f35a600ad01e0a795e64035b8144292f275
Laura <laura.e.vollmer@gmail.com>
2024-07-18 09:23:01 -0500

    updating tailwind config js with colors and assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        White: "0, 0%, 100%",
+        "light-pink": "275, 100%, 97%",
+        "grayish-purple": "292, 16%, 49%",
+        "dark-purple": "292, 42%, 14%",
+      },
+    },
   },
   plugins: [],
-}
-
+};
