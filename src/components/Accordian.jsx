cdfa3100db1d9a7d0b0932d1ad945448f15985d6
Laura <laura.e.vollmer@gmail.com>
2024-07-18 12:24:07 -0500

    updating accordian

--- src/components/Accordian.jsx
+++ src/components/Accordian.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Accordian = () => {
   return (
-    <div>Accordian</div>
+    <div id="accordion-collapse" data-accordion="collapse">
+      <h2 id="accordion-collapse-heading-1">
+        <button type="button" className='flex items-center justify-between w-full p-5 font-medium trl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 '></button>
+      </h2>
+    </div>
   )
 }
 
