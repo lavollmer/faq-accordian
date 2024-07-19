7033705ed75b5755cc4c799c7647b19ff385276f
Laura <laura.e.vollmer@gmail.com>
2024-07-19 07:53:58 -0500

    styling the accordian

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import "./App.css";
 import "./index.css";
-import Accordian from "./components/Accordian.jsx"
+import Accordian from "./components/Accordian.jsx";
 
 function App() {
   return (
     <>
       <div className="background-container">
-        <div className="image-background"></div>
-        <div className="color-background"></div>
-      </div>
-      <div>
-        <Accordian />
+        <div className="image-background z-10"></div>
+        <div className="color-background z-10">
+          <Accordian className="absolute z-50" />
+        </div>
       </div>
     </>
   );
