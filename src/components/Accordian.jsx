ca7408b824f296cfa9b04067ad6c92af26d1857c
Laura <laura.e.vollmer@gmail.com>
2024-07-19 08:04:35 -0500

    changing arrows to plus signs and cleaning up code

--- src/components/Accordian.jsx
+++ src/components/Accordian.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import iconStar from "../assets/icon-star.svg";
+import iconPlus from "../assets/icon-plus.svg";
+import iconMinus from "../assets/icon-minus.svg";
 
 const Accordian = () => {
   return (
-    <div className="bg-white font-worksans">
+    <div className="flex flex-col bg-white font-worksans">
       <div className="flex flex-row justify-start">
-        <img
-          src={iconStar}
-          alt="icon-star"
-        />
+        <img src={iconStar} alt="icon-star" />
         <h1 className="font-bold text-dark-purple text-4xl">FAQS</h1>
       </div>
-      <div id="accordion-collapse" data-accordion="collapse" className="font-worksans">
+      <div
+        id="accordion-collapse"
+        data-accordion="collapse"
+        className="font-worksans"
+      >
         <h2 id="accordion-collapse-heading-1">
           <button
             type="button"
@@ -20,23 +23,26 @@ const Accordian = () => {
             aria-expanded="true"
             aria-controls="accordion-collapse-body-1"
           >
-            <span>What is Frontend Mentor, and how will it help me?</span>
-            <svg
+            <span className="text-dark-purple">
+              What is Frontend Mentor, and how will it help me?
+            </span>
+            {/* <svg
               data-accordion-icon
               className="w-3 h-3 rotate-180 shrink-0"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 10 6"
-            >
-              <path
+            > */}
+              <img src={iconPlus} alt="plus icon" />
+              {/* <path
                 stroke="currentColor"
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
                 d="M9 5 5 1 1 5"
               />
-            </svg>
+            </svg> */}
           </button>
         </h2>
         <div
@@ -46,18 +52,10 @@ const Accordian = () => {
         >
           <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
             <p className="mb-2 text-gray-500 dark:text-gray-400">
-            Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.
-            </p>
-            <p className="text-gray-500 dark:text-gray-400">
-              Check out this guide to learn how to{" "}
-              <a
-                href="/docs/getting-started/introduction/"
-                className="text-blue-600 dark:text-blue-500 hover:underline"
-              >
-                get started
-              </a>{" "}
-              and start developing websites even faster with components on top
-              of Tailwind CSS.
+              Frontend Mentor offers realistic coding challenges to help
+              developers improve their frontend coding skills with projects in
+              HTML, CSS, and JavaScript. It's suitable for all levels and ideal
+              for portfolio building.
             </p>
           </div>
         </div>
@@ -69,23 +67,9 @@ const Accordian = () => {
             aria-expanded="false"
             aria-controls="accordion-collapse-body-2"
           >
-            <span>Is Frontend Mentor free?</span>
-            <svg
-              data-accordion-icon
-              className="w-3 h-3 rotate-180 shrink-0"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 10 6"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 5 5 1 1 5"
-              />
-            </svg>
+            <span className="text-dark-purple">Is Frontend Mentor free?</span>
+            <img src={iconPlus} alt="plus icon" />
+
           </button>
         </h2>
         <div
@@ -95,19 +79,11 @@ const Accordian = () => {
         >
           <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
             <p className="mb-2 text-gray-500 dark:text-gray-400">
-            Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.
+              Yes, Frontend Mentor offers both free and premium coding
+              challenges, with the free option providing access to a range of
+              projects suitable for all skill levels.
             </p>
-            <p className="text-gray-500 dark:text-gray-400">
-              Check out the{" "}
-              <a
-                href="https://flowbite.com/figma/"
-                className="text-blue-600 dark:text-blue-500 hover:underline"
-              >
-                Figma design system
-              </a>{" "}
-              based on the utility classNamees from Tailwind CSS and components
-              from Flowbite.
-            </p>
+
           </div>
         </div>
         <h2 id="accordion-collapse-heading-3">
@@ -118,25 +94,10 @@ const Accordian = () => {
             aria-expanded="false"
             aria-controls="accordion-collapse-body-3"
           >
-            <span>
-            Can I use Frontend Mentor projects in my portfolio?
+            <span className="text-dark-purple">
+              Can I use Frontend Mentor projects in my portfolio?
             </span>
-            <svg
-              data-accordion-icon
-              className="w-3 h-3 rotate-180 shrink-0"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 10 6"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 5 5 1 1 5"
-              />
-            </svg>
+            <img src={iconPlus} alt="plus icon" />
           </button>
         </h2>
         <div
@@ -146,35 +107,10 @@ const Accordian = () => {
         >
           <div className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
             <p className="mb-2 text-gray-500 dark:text-gray-400">
-            Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!
+              Yes, you can use projects completed on Frontend Mentor in your
+              portfolio. It's an excellent way to showcase your skills to
+              potential employers!
             </p>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">
-              However, we actually recommend using both Flowbite, Flowbite Pro,
-              and even Tailwind UI as there is no technical reason stopping you
-              from using the best of two worlds.
-            </p>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">
-              Learn more about these technologies:
-            </p>
-            <ul className="ps-5 text-gray-500 list-disc dark:text-gray-400">
-              <li>
-                <a
-                  href="https://flowbite.com/pro/"
-                  className="text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Flowbite Pro
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://tailwindui.com/"
-                  rel="nofollow"
-                  className="text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Tailwind UI
-                </a>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
